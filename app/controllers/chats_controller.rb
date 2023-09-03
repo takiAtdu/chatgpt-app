96d9ab3a5b76654574e135cc4dda374344174761
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-09-03 15:54:26 +0900

    [add]whisper

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,52 +6,20 @@ class ChatsController < ApplicationController
   def form_submitted
     @chat = Chat.new(chat_params)
     accepted_format = [".mp3"]
-    if accepted_format.include?(@chat.mp3_file)
+    # if accepted_format.include?(@chat.mp3_file)
       if @chat.save
-        redirect_to action: :form
+        text = transcribe(@chat)
+        # @chat.update(transcibed_text: "hello")
+        @chat.update(transcibed_text: text)
+        redirect_to action: :show, id: @chat.id
       end
-    else
-      redirect_to action: :form
-    end
+    # else
+    #   redirect_to action: :form
+    # end
   end
 
   def show
     @chat = Chat.find(params[:id])
-  end
-
-
-
-  def index
-    client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
-    response = client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [
-          { role: "user", content: "hello" }
-        ],
-      },
-    )
-
-    @message = response.dig("choices", 0, "message", "content")
-  end
-
-  def input
-  end
-
-  def output
-    input = params[:input]
-
-    client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
-    response = client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [
-          { role: "user", content: input }
-        ],
-      },
-    )
-
-    @output = response.dig("choices", 0, "message", "content")
   end
 
   def chat
@@ -74,6 +42,28 @@ class ChatsController < ApplicationController
 
   private
     def chat_params
-      params.require(:chat).permit(:mp3_file)
+      params.require(:chat).permit(:id, :mp3_file, :transcibed_text, :summarized_text)
+    end
+
+    def create_tempfile(chat)
+      tempfile = Tempfile.open("temp")
+      tempfile.write(chat.mp3_file)
+      tempfile.rewind
+
+      return tempfile
+    end
+
+    # 音声ファイルを文字起こしし、データを返却
+    def transcribe(chat)
+      tempfile = create_tempfile(chat)
+
+      client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
+      response = client.transcribe(
+      parameters: {
+          model: "whisper-1",
+          file: File.open(tempfile, 'rb'),
+      })
+
+      return response
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root "application#index"
 
-  get 'chats/index', to: 'chats#index'
-  get 'chats/input', to: 'chats#input'
-  get 'chats/output/', to: 'chats#output'
   get 'chats/chat', to: 'chats#chat'
   get 'chats/form', to: 'chats#form'
   post 'chats/form_submitted', to: 'chats#form_submitted'
