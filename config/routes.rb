e2507bbd701c83c4a7c5060a77394790aefcf689
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2023-09-03 17:28:16 +0900

    [fix]change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "application#index"
+  root 'chats#chat'
 
-  get 'chats/chat', to: 'chats#chat'
   get 'chats/form', to: 'chats#form'
   post 'chats/form_submitted', to: 'chats#form_submitted'
   get 'chats/show/:id', to: 'chats#show'
